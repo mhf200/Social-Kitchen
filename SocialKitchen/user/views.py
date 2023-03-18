8a30a3c8b9fcecbd2160b40478da0206a9b82cda
Ali Faour <aaf36@mail.aub.edu>
2023-03-18 21:49:13 +0200

    added form in register.html

--- SocialKitchen/user/views.py
+++ SocialKitchen/user/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
-    return render(request, "user/register.html")
+    Form =UserCreationForm
+    if request.method=="post" :
+        Form =UserCreationForm(request.post)
+        if Form.is_valid():
+            Form.save
+            
+    return render(request, "user/register.html", {
+        "form":Form
+    })
