29261af387c76258333796ec5674f45da9587e5e
Ali Faour <aaf36@mail.aub.edu>
2023-03-19 22:28:23 +0200

    added styling to registration form and added static directory

--- SocialKitchen/user/forms.py
+++ SocialKitchen/user/forms.py
@@ -2,12 +2,20 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
 class UserRegistrationForm(UserCreationForm):
-    first_name=forms.CharField(max_length=100)
-    last_name=forms.CharField(max_length=100)
-    email=forms.EmailField(label="Email")
+    username = forms.CharField(max_length=30, required=True, help_text='required' )
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    email = forms.EmailField(max_length=254, required=True, help_text='Required. Enter a valid email address.')
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2'] 
+        fields = ['username','first_name', 'last_name', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('This email address is already in use.')
+        return email
--- SocialKitchen/user/urls.py
+++ SocialKitchen/user/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from . import views
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from user import views as user_views
+
 
 urlpatterns= [
-    path("register", user_views.index, name="register"),
-    path("admin/", admin.site.urls)
+    path("register", views.index, name="register")
 ]
