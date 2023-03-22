1521d13d65efe4656103264b5752f4af4e83a7e7
jawad <mhf24@mail.aub.edu>
2023-03-22 20:51:36 +0200

    update forms.py

--- SocialKitchen/user/forms.py
+++ SocialKitchen/user/forms.py
@@ -1,21 +1,8 @@
-from django import forms
+from django import forms 
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import User
 
-class UserRegistrationForm(UserCreationForm):
-    username = forms.CharField(max_length=30, required=True, help_text='required' )
-    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
-    last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
-    email = forms.EmailField(max_length=254, required=True, help_text='Required. Enter a valid email address.')
-    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
-
+class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username','first_name', 'last_name', 'email', 'password1', 'password2']
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('This email address is already in use.')
-        return email
+        fields = ['username', 'email', 'password1', 'password2']
