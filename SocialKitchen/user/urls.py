28220d930b9c5bc11267018654df3d9b2acbc348
jawad <mhf24@mail.aub.edu>
2023-03-22 20:48:30 +0200

    update urls.py

--- SocialKitchen/user/urls.py
+++ SocialKitchen/user/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from . import views
 
+app_name='user'
 
-urlpatterns= [
-    path("register", views.index, name="register")
+urlpatterns = [
+    path('loginpage/', views.loginPage, name="loginpage"),
+    path('registerpage/', views.registerPage, name="registerpage"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('', views.home, name='home'),
+    path('recipes/',views.recipes , name='recipes') ,
+    path ( 'recipes/<int:id>/', views.recipe_detail , name='recipe_detail' ),
+    path('recipes/add', views.add_recipe, name='add_recipe'),
+    path('recipes/update/<int:id>/' , views.update_recipe , name='update_recipe'),
+    path('recipes/delete/<int:id>/' , views.delete_recipe , name='delete_recipe'),
+    path ('index',views.index , name='index'),
+    
 ]
